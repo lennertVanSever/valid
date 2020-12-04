011c84e9e065211a5676ca9847d92bde409c33f1
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2020-12-04 23:18:14 +0100

    add issuer endpoint

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = async ({ subject, text, res, req }) => {
-  const msg = {
-    to: req.body.event.data.new.email,
-    from: "lennert_vansever@hotmail.be", // Use the email address or domain you verified above
-    subject,
-    text
-  };
-
   try {
+    const msg = {
+      to: req.body.event.data.new.email,
+      from: "lennert_vansever@hotmail.be", // Use the email address or domain you verified above
+      subject,
+      text
+    };
+
     const emailSentResponse = await sgMail.send(msg);
     res.json({ msg, emailSentResponse });
   } catch (error) {
@@ -23,7 +23,7 @@ const sendMail = async ({ subject, text, res, req }) => {
 
     if (error.response) {
       console.error(error.response.body);
-      res.json({ error: error.response.body });
+      res.json({ error });
     }
   }
 };
@@ -32,24 +32,30 @@ app.set("port", process.env.PORT || 3000);
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "no-cache");
   next();
 });
 
-app.post("/confirm/administrator", function(req, res) {
-  try {
-    sendMail({
-      subject: "New administrator for: xxxx",
-      text: `Hi, You can now start creating issuers on LINK `,
-      res,
-      req
-    });
-  } catch (error) {
-    res.json({ error: error });
-  }
+app.post("/confirm/administrator", function (req, res) {
+  sendMail({
+    subject: "New administrator for: xxxx",
+    text: `Hi, You can now start creating issuers on LINK `,
+    res,
+    req
+  });
 });
-app.listen(app.get("port"), function() {
+
+app.post("/confirm/issuer", function (req, res) {
+  sendMail({
+    subject: "New issuer for: xxxx",
+    text: `Hi, You can now start creating certificates on LINK `,
+    res,
+    req
+  });
+});
+
+app.listen(app.get("port"), function () {
   console.log("Server started on: " + app.get("port"));
 });
